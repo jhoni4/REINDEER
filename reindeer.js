d04bcea769370a77150a47b04cf1636b669ee144
yonatan molla <jhoni42002@yahoo.com>
2016-07-24 15:50:56 -0500

    sumbitted reindeer exercise

--- reindeer.js
+++ reindeer.js
@@ -1,7 +1,6 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
-
 var coloredReindeer = "";
 
 // i changed the id from "coloredReindeer" to "Reindeer".
